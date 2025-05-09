e814fc99537c6c18bfde8c4bf72eb2a67be942ef
mirkarlar <mirkarlar@gmail.com>
2025-05-09 11:40:45 +0200

    basic project structure and spring setup

---
+++ src/main/java/nl/hilgenbos/psyichic_pies/PsyichicPiesApplication.java
@@ -0,0 +1,13 @@
+package nl.hilgenbos.psyichic_pies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsyichicPiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsyichicPiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/hilgenbos/psyichic_pies/PsyichicPiesApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hilgenbos.psyichic_pies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsyichicPiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
